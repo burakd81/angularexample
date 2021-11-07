f9e69e4c5b19cadeccb0f3321b638ad4798d8674
Burak Demirer <65796394+burakd81@users.noreply.github.com>
2021-11-07 16:56:54 +0300

    Add files via upload

---
+++ src/app/services/card.service.ts
@@ -0,0 +1,33 @@
+import {Inject, Injectable} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Card} from "../models/card";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardService {
+
+  cards!:Card[];
+  constructor(
+    @Inject('apiURL') private apiURL:string,
+    private http:HttpClient
+  ) { }
+
+  getCards():void {
+
+     this.http.get<Card[]>(this.apiURL+'/cards')
+       .subscribe((res:any)=>{
+         this.cards=res;
+       })
+  }
+  addCard(card:Card):Observable<any>{
+    return this.http.post(this.apiURL+'/cards',card);
+  }
+  updateCard(card:Card,cardId:number):Observable<any>{
+    return this.http.put(this.apiURL+'/cards/'+cardId,card);
+  }
+  deleteCard(cardId:number):Observable<any>{
+    return this.http.delete(this.apiURL+'/cards/'+cardId)
+  }
+}
